e388ff33bab06c9d38cc22fb4cfd67743d7b3844
Lahtela Risto S <ristolah@da5-cs-bk107-14.pc.helsinki.fi>
2018-01-26 14:57:10 +0200

    ex 1.7

--- week1/ex6/src/App.js
+++ week1/ex6/src/App.js
@@ -15,16 +15,39 @@ class Stats extends Component {
     }
   }
 
-  hyva = () => () => this.setState({hyva: this.state.hyva + 1});
-  neutraali = () => () => this.setState((prevState) => (
-    {neutraali: prevState.neutraali + 1}
-  ));
-  huono = () => () => this.setState({huono: this.state.huono + 1});
+  hyva = () => () => {
+    this.setState({hyva: this.state.hyva + 1});
+
+  }
+  neutraali = () => () => {
+    this.setState((prevState) => ({neutraali: prevState.neutraali + 1}));
+
+  }
+  huono = () => () => {
+    this.setState({huono: this.state.huono + 1});
+  }
+
+  laskeKeskiarvo = () => {
+    const good = this.state.hyva
+    const neut = this.state.neutraali
+    const bad = this.state.huono
+    const total = good + neut + bad
+    return total === 0 ? 0 : ((good - bad) / total).toFixed(2)
+  }
+
+  laskePositiiviset = () => {
+    const good = this.state.hyva
+    const neut = this.state.neutraali
+    const bad = this.state.huono
+    const total = good + neut + bad
+    return total === 0 ? 0 : ((good / total) * 100).toFixed(2)
+  }
 
   render() {
     const hyva = this.state.hyva
     const neutraali = this.state.neutraali
     const huono = this.state.huono
+    const keskiarvo = this.state.keskiarvo
     return <div>
         <Nappi nimi="Hyvä" funk={this.hyva()} />
         <Nappi nimi="Neutraali" funk={this.neutraali()} />
@@ -33,6 +56,8 @@ class Stats extends Component {
         <p>Hyvä: {hyva}</p>
         <p>Neutraali: {neutraali}</p>
         <p>Huono: {huono}</p>
+        <p>Keskiarvo: {this.laskeKeskiarvo()}</p> 
+        <p>Positiivisia: {this.laskePositiiviset()}%</p>
       </div>
   }
 }
