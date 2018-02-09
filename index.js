528b4aba240a5bf8d410698e644459c581943fe8
Rsl1122 <djrapitops@hotmail.com>
2018-02-09 15:40:14 +0200

    Added static serving to backend

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
-
-
-app.get('/', (req, res) => {
-    res.send('<p>No API endpoint in this address.</p>')
-})
+app.use(express.static('build'))
 
 let persons = [
     {
