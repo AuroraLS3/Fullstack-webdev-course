3c9ca96f246d3a33b03983f02a2a6a6a5debb8d0
Rsl1122 <djrapitops@hotmail.com>
2018-03-06 10:42:17 +0200

    Fixed create functionality

--- part6/anecdotes-redux/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux/src/components/AnecdoteForm.js
@@ -1,20 +1,33 @@
 import React from 'react'
 
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { notify, hide } from '../reducers/notificationReducer'
 
 class AnecdoteForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
   handleSubmit(e) {
     e.preventDefault()
     const content = e.target.anecdote.value
+
     this.props.store.dispatch(createAnecdote(content))
+    this.props.store.dispatch(notify(`Created new Anecdote: '${content}'`))
+    setTimeout(() => this.props.store.dispatch(hide()), 5000)
 
     e.target.anecdote.value = ''
   }
+
   render() {
     return (
       <div>
         <h2>create new</h2>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={(event) => {
+          this.handleSubmit.bind(this)
+          this.handleSubmit(event)
+        }}>
           <div><input name='anecdote' /></div>
           <button>create</button>
         </form>
